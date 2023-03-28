93bb6b4a64fc472e86ef6a66dd9c5caf0d3bfec2
DaronBecaus <danielsilvasoder@gmail.com>
2023-03-28 05:35:35 -0300

    Aula Introdutória de Peyton

---
+++ main.py
@@ -0,0 +1,91 @@
+# print("Hello World")
+
+# Casting
+# str -> string
+# int -> inteiro
+# float -> decima
+
+# x = str(3)
+# y = int(3)
+# z = float(3)
+
+# print(type(x), type(y), type(z))
+
+# minhaVariável = 5
+# print("O valor é:", minhaVariável)
+
+# outraVariável = 10
+# print(minhaVariável, outraVariável)
+
+# nome = str(input("Escreva seu nome: "))
+# print("Bem vindo", nome)
+
+# a = 333
+# b = 33
+# if b > a:
+#     print("b é maior que a")
+# elif b == a:
+#     print("os dois são iguais")
+# else:
+#     print("a é maior que b")
+
+# idade = 20
+
+# if idade >= 18 and idade < 70:
+#     print("Voto Obrigatório")
+
+
+# idade = int(input("Digite sua idade: "))
+
+# if idade >= 18 and idade < 70:
+#     print("Seu voto é obrigatório.")
+# elif idade >= 16 or idade > 70:
+#     print("Seu voto é opcional.")
+# else:
+#     print("Você não pode votar")
+
+# valor = int(input("Digite um valor: "))
+
+# if valor > 10:
+#     print("O valor inserido esta acima de 10, ")
+#     if valor > 20:
+#         print("Também esta acima de 20")
+#     else:
+#         print("Mas não esta acima de 20")
+
+# i = 1
+
+# while i < 6:
+#     print(i)
+#     i += 1  # isso é igual a i = i+1
+
+# criando uma lista
+# frutas = ["maçã", "banana", "morango"]
+# frutas.sort()
+# for fruta in frutas:
+#     print(fruta)
+
+# print(frutas[0])
+# frutas[0] = "laranja"
+# print(frutas[0])
+
+# criando um dicionario
+
+# pessoa = {
+#     "nome": "Daniel",
+#     "idade": 29
+# }
+
+# # acessando o valor pela chave
+# print(pessoa["nome"])
+# # trocando o valor pela chave
+# pessoa["nome"] = "Luis"
+# print(pessoa["nome"])
+
+# criando função
+def minhaFunção():
+    print("Ola Mundo")
+
+
+# chamando a função
+minhaFunção
